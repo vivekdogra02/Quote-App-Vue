0a1174ceed72961eceed07f7e77b9630f410e5c5
Vivek Dogra <vivekdogra02@gmail.com>
2018-05-13 01:41:39 +0530

    Quote Application in Vue

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div  class="container">
+      <app-header :quoteCount="quotes.length" :maxQuote="maxQuotes"></app-header>
+      <new-quote @quoteAdded="newQuote"></new-quote>
+      <quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">
+            Click on the Quote to delete it.
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid';
+import NewQuote from './components/NewQuote';
+import Header from './components/Header';
+export default {
+  data: function() {
+    return {
+      quotes: ['Just to see'],
+      maxQuotes: 10
+    }
+  },
+  methods: {
+    newQuote(quote) {
+      if(this.quotes.length >= this.maxQuotes) {
+        return 
+      }
+      this.quotes.unshift(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index,1); 
+    }
+  },
+  components: {
+    quoteGrid: QuoteGrid,
+    newQuote: NewQuote,
+    appHeader: Header
+  }
+}
+</script>
+
+
+<style lang="scss">
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <div class="row">
+      <div class="col-sm-12">
+          <h3>Quote Added</h3>
+      <div class="progress">
+          <div class="progress-bar" role="progressbar" 
+          aria-valuemin="0"
+          aria-valuenow="40"
+          aria-valuemax="100"
+          :style="{width: (quoteCount/maxQuote) * 100 + '%'}">
+        {{quoteCount}} / {{maxQuote}}
+          </div>
+      </div>
+      </div>
+
+  </div>
+  <div class="row" v-if="quoteCount >= maxQuote">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-danger">
+            You can't add more than {{maxQuote}} Quotes.
+            Please Delete and then add.
+          </div>
+        </div>
+</div>
+</div>
+  
+</template>
+<script>
+export default {
+  props: ['quoteCount', 'maxQuote']
+}
+</script>
+
+<style scoped>
+
+</style>
+
